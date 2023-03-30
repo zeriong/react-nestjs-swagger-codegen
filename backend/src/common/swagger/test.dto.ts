009ffb91ab7dc32d83867cd1aacf91e63e05e703
zeriong <jaeryong95@gmail.com>
2023-03-30 21:40:19 +0900

    openApi.ts 추가하여 OpenApiNestFactory 분리설정

--- backend/src/common/swagger/test.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class testId {
-  @ApiProperty({
-    example: 15,
-  })
-  sub: number;
-}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
-import { setupSwagger } from './common/swagger';
+import { useOpenApi } from './openApi';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  setupSwagger(app);
+  await useOpenApi(app);
 
   app.enableCors({
     origin: 'http://localhost:3000',
